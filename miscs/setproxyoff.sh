c37c8292d10350763b727eb5126173f22194b5e6
Teruaki Yokoyama <tel1203@gmail.com>
2023-05-23 22:49:24 +0900

    Modified memo and scripts on MBAir2023. This is for MBPr2023.

---
+++ miscs/setproxyoff.sh
@@ -0,0 +1,5 @@
+networksetup -setwebproxystate Wi-Fi off
+networksetup -setsecurewebproxystate Wi-Fi off
+networksetup -getwebproxy Wi-Fi
+networksetup -getsecurewebproxy Wi-Fi
+
---
+++ miscs/setproxyon.sh
@@ -0,0 +1,7 @@
+networksetup -setwebproxy Wi-Fi localhost 3128
+networksetup -setsecurewebproxy Wi-Fi localhost 3128
+networksetup -setwebproxystate Wi-Fi on
+networksetup -setsecurewebproxystate Wi-Fi on
+networksetup -getwebproxy Wi-Fi
+networksetup -getsecurewebproxy Wi-Fi
+
